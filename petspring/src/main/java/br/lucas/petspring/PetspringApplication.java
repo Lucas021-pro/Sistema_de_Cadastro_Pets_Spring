dbf7f25de731939d6236753998290688fe8ee27a
Lucas Eduardo <lucaseduardo0621@gmail.com>
2026-05-18 15:56:32 -0300

    chore: estrutura inicial do projeto do spring initializer

---
+++ petspring/src/main/java/br/lucas/petspring/PetspringApplication.java
@@ -0,0 +1,13 @@
+package br.lucas.petspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetspringApplication.class, args);
+	}
+
+}
---
+++ petspring/src/test/java/br/lucas/petspring/PetspringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.lucas.petspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
