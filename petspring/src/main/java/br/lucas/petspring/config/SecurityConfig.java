f829d510346ef4abdd21f8b6eb497d1492392521
Lucas Eduardo <lucaseduardo0621@gmail.com>
2026-07-01 16:46:02 -0300

    security: configura spring security para apis stateless e define regras de autorizacao baseadas em metodos HTTP

--- petspring/src/main/java/br/lucas/petspring/config/SecurityConfig.java
+++ petspring/src/main/java/br/lucas/petspring/config/SecurityConfig.java
@@ -2,9 +2,11 @@ package br.lucas.petspring.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,8 +17,15 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .headers(headers -> headers
+                                .frameOptions(frame -> frame.deny())
+                )
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll()
+                        .requestMatchers(HttpMethod.GET, "/v1/pets/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/v1/donos/**").permitAll()
+                        .anyRequest().authenticated()
                 );
 
         return http.build();
